2dcf8202b0de1ec17c60d9538dd43faa2dd3bab1
Muhibollah Khandaker <muhebollah.diu@gmail.com>
2020-05-29 21:29:29 +0600

    update sell page

---
+++ InventoryManagement.Web/wwwroot/page-js/product-selling.js
@@ -0,0 +1,37 @@
+﻿
+//date picker
+$('.datepicker').pickadate().val(moment(new Date()).format('DD MMMM, YYYY'));
+document.getElementById('label-date').classList.add('active');
+
+ // Material Select Initialization
+$('.mdb-select').materialSelect();
+
+
+//*****SELECTORS***//
+
+//product code form
+const formCode = document.getElementById('formCode')
+const inputBarCode = formCode.inputBarCode
+
+//****FUNCTIONS****//
+
+//scan barcode
+onScan.attachTo(document, {
+    suffixKeyCodes: [13],
+    reactToPaste: true,
+    onScan: (sCode, iQty) => {
+        inputBarCode.value = sCode
+        inputBarCode.nextElementSibling.classList.add('active')
+    }
+})
+
+//event listners
+formCode.addEventListener('submit', evt => {
+    evt.preventDefault()
+    console.log('form submitted')
+})
+
+
+
+
+//****CUSTOMER****//
