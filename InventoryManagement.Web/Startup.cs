d670ad660426bd6e968b460d57093f7748e3327c
muhebollah <muhebollah.diu@gamil.com>
2020-06-11 13:33:58 +0600

    some change

--- InventoryManagement.Web/Startup.cs
+++ InventoryManagement.Web/Startup.cs
@@ -52,7 +52,14 @@ namespace InventoryManagement.Web
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
