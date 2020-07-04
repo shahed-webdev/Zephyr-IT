307ea3afcd009056d8b1bea9d5e2cfa3446a640a
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2020-07-04 18:32:20 +0600

    Customer Profile

--- InventoryManagement.Repository/Repositories/Customer/CustomerViewModel.cs
+++ InventoryManagement.Repository/Repositories/Customer/CustomerViewModel.cs
@@ -55,4 +55,39 @@ namespace InventoryManagement.Repository
         public string CustomerAddress { get; set; }
         public string CustomerPhone { get; set; }
     }
+
+    public class CustomerProfileViewModel
+    {
+        public CustomerProfileViewModel()
+        {
+            PhoneNumbers = new HashSet<CustomerPhoneViewModel>();
+            SellingRecords = new HashSet<CustomerSellingViewModel>();
+        }
+        public int CustomerId { get; set; }
+        public string OrganizationName { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string Description { get; set; }
+        public double DueLimit { get; set; }
+        public string Designation { get; set; }
+        public bool IsIndividual { get; set; }
+        public byte[] Photo { get; set; }
+        public DateTime SignUpDate { get; set; }
+        public ICollection<CustomerPhoneViewModel> PhoneNumbers { get; set; }
+        public ICollection<CustomerSellingViewModel> SellingRecords { get; set; }
+        public double SoldAmount { get; set; }
+        public double ReceivedAmount { get; set; }
+        public double DiscountAmount { get; set; }
+        public double DueAmount { get; set; }
+    }
+    public class CustomerSellingViewModel
+    {
+        public int SellingId { get; set; }
+        public int SellingSn { get; set; }
+        public double SellingAmount { get; set; }
+        public double SellingPaidAmount { get; set; }
+        public double SellingDiscountAmount { get; set; }
+        public double SellingDueAmount { get; set; }
+        public DateTime SellingDate { get; set; }
+    }
 }
