ed43715921bd45a79a86e3d13ec37c3cd06d2295
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2020-07-08 10:43:58 +0600

    Expense Error sloved

---
+++ InventoryManagement.Data/Migrations/20200708044323_UpdateExpense.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InventoryManagement.Data.Migrations
+{
+    public partial class UpdateExpense : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpenseDate",
+                table: "Expense",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldDefaultValueSql: "(getdate())");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpenseDate",
+                table: "Expense",
+                type: "date",
+                nullable: false,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+    }
+}
