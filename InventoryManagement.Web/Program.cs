d601ad132933df1f035ce83b23da83d7914d6652
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2020-09-28 20:47:12 +0600

    Auto migration to server

--- InventoryManagement.Web/Program.cs
+++ InventoryManagement.Web/Program.cs
@@ -1,4 +1,7 @@
+using InventoryManagement.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace InventoryManagement.Web
@@ -7,7 +10,15 @@ namespace InventoryManagement.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
