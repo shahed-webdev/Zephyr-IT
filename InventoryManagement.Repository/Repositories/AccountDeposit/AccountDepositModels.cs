fec284776c964f40c24a25a0211d0c8438b412b6
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2021-03-06 10:33:14 +0600

    Account CRUD model created

---
+++ InventoryManagement.Repository/Repositories/AccountDeposit/AccountDepositModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InventoryManagement.Repository  
+{
+    public class AccountDepositCrudModel
+    {
+        public int AccountDepositId { get; set; }
+        public int AccountId { get; set; }
+        public decimal DepositAmount { get; set; }
+        public string Description { get; set; }
+        public DateTime DepositDateUtc { get; set; }
+
+    }
+}
