afad6d0066efe7d97233e95a641a084521a41670
MUHIBOLLAH <muhebollah.diu@gmail.com>
2021-06-13 12:54:51 +0600

    find damage , list page added

--- InventoryManagement.Web/Controllers/DamageController.cs
+++ InventoryManagement.Web/Controllers/DamageController.cs
@@ -3,12 +3,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InventoryManagement.Repository;
+using Microsoft.AspNetCore.Authorization;
 
 namespace InventoryManagement.Web.Controllers
 {
+    [Authorize]
     public class DamageController : Controller
     {
-        public IActionResult Index()
+        private readonly IUnitOfWork _db;
+        public DamageController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+
+        #region Add Damage
+        [Authorize(Roles = "admin, damage-find-product")]
+        public IActionResult SearchProduct()
+        {
+            return View();
+        }
+
+        //call from ajax
+        public async Task<IActionResult> FindUnsoldProduct(string code)
+        {
+            var data = await _db.ProductStocks.FindforDetailsAsync(code).ConfigureAwait(false);
+            return Json(data);
+        }
+        #endregion
+
+        [Authorize(Roles = "admin, damaged-list")]
+        public IActionResult DamagedList()
         {
             return View();
         }
