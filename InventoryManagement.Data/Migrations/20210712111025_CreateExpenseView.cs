24e45fdfc9a6e3f7bd97b453a93938d6e2c957a2
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2021-07-16 12:03:48 +0600

    SQL view change

--- InventoryManagement.Data/Migrations/20210712111025_CreateExpenseView.cs
+++ InventoryManagement.Data/Migrations/20210712111025_CreateExpenseView.cs
@@ -13,7 +13,7 @@ FROM  Expense INNER JOIN
 Registration ON Expense.RegistrationId = Registration.RegistrationId INNER JOIN
 ExpenseCategory ON Expense.ExpenseCategoryId = ExpenseCategory.ExpenseCategoryId
 UNION ALL
-SELECT ExpenseTransportation.ExpenseTransportationId AS Id, ExpenseTransportation.VoucherNo, ExpenseTransportation.IsApproved, CAST(0 AS bit) AS IsTransportation, Registration.UserName AS CreateBy, 'Transportation' AS ExpenseCategory, ExpenseTransportation.TotalExpense, ExpenseTransportation.ExpenseNote, ExpenseTransportation.ExpenseDate
+SELECT ExpenseTransportation.ExpenseTransportationId AS Id, ExpenseTransportation.VoucherNo, ExpenseTransportation.IsApproved, CAST(1 AS bit) AS IsTransportation, Registration.UserName AS CreateBy, 'Transportation' AS ExpenseCategory, ExpenseTransportation.TotalExpense, ExpenseTransportation.ExpenseNote, ExpenseTransportation.ExpenseDate
 FROM ExpenseTransportation INNER JOIN
 Registration ON ExpenseTransportation.RegistrationId = Registration.RegistrationId");
         }
